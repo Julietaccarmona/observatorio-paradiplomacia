1207d1a4c6675492c40fb11658ce20c2ea638145
Julieta <julieta_carmona_99@hotmail.com>
2026-06-14 17:20:50 -0300

    Cambio a postgresql como base de datos principal

--- core/models.py
+++ core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Provincia(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Actor(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Convenio(models.Model):
+    provincia = models.ForeignKey(Provincia, on_delete=models.CASCADE)
+    pais = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+    anio = models.IntegerField()
+    descripcion = models.TextField()
+
+    def __str__(self):
+        return f"{self.provincia} - {self.pais} ({self.anio})"
--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Convenio
+
+def lista_convenios(request):
+    convenios = Convenio.objects.all()
+    return render(request, 'convenios.html', {'convenios': convenios})
